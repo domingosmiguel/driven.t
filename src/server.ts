47105558ec63d428b4b446c1ca97316b2b075d77
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2023-01-30 17:28:41 -0300

    fix: server duplication

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,3 @@ init().then(() => {
     console.log(`Server is listening on port ${port}.`);
   });
 });
-
-const port = +process.env.PORT || 4000;
-
-init().then(() => {
-  app.listen(port, () => {
-    /* eslint-disable-next-line no-console */
-    console.log(`Server is listening on port ${port}.`);
-  });
-});
